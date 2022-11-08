50cf35d31bd14101a58a46d17fbade18910eea75
JoaquinGimenez-112733 <101760750+JoaquinGimenez-112733@users.noreply.github.com>
2022-11-08 19:06:41 -0300

    update dbb

--- back/src/main/java/Models/EstadisticasDTO.java
+++ back/src/main/java/Models/EstadisticasDTO.java
@@ -1,21 +1,21 @@
-package Models;
-
-public class EstadisticasDTO {
-    //estadisticas personales
-    private int rachaVictorias;
-    private int cantBJ;
-    private int cantPartidas;
-    private int cantMaxCartas;
-
-    private int victorias;
-    private int derrotas;
-    private int empates;
-
-    //estadisticas globales
-
-    private float porcentajeW;
-    private float porcentajeL;
-    private float porcentajeT;
-
-
-}
+//package Models;
+//
+//public class EstadisticasDTO {
+//    //estadisticas personales
+//    private int rachaVictorias;
+//    private int cantBJ;
+//    private int cantPartidas;
+//    private int cantMaxCartas;
+//
+//    private int victorias;
+//    private int derrotas;
+//    private int empates;
+//
+//    //estadisticas globales
+//
+//    private float porcentajeW;
+//    private float porcentajeL;
+//    private float porcentajeT;
+//
+//
+//}
